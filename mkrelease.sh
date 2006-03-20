fe50396b3847c9a32d03d3e3d6affd33642edd8c
todsah <todsah@05a960da-bb1a-49f1-bb6f-184d92f9ea5e>
2006-03-20 16:27:25 +0000

    Initial commit.
    git-svn-id: https://svn.electricmonk.nl/svn/tlib-php/trunk@12 05a960da-bb1a-49f1-bb6f-184d92f9ea5e

---
+++ mkrelease.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+if [ -z $1 ]; then
+    echo "Usage: mkrelease.sh <version>"
+    exit;
+fi
+
+# Pre-clean
+rm tlib-php-$1.tar.gz
+rm tlib-php-$1-win.zip
+
+# Documentation
+doxygen
+
+# Unix (src) release
+mkdir tlib-php-$1
+cp * tlib-php-$1
+cp html tlib-php-$1 -r
+rm tlib-php-$1/mkrelease.sh
+rm tlib-php-$1/Doxyfile
+tar -vczf tlib-php-$1.tar.gz tlib-php-$1
+
+# Cleanup.
+rm tlib-php-$1 -r
